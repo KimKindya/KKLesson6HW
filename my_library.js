958b17f23a9d7fe47aed815affdf061f73db5529
Kim Kindya <kakindya@gmail.com>
2015-10-10 22:50:36 -0400

    Added header to HTML page. Made header show/hide in the js file

---
+++ my_library.js
@@ -0,0 +1,36 @@
+// This page demonstrates JQUery anonymousfunctons and events.
+// It's also a cheat sheet.
+
+// THIS IS VERY IMPORTANT to have first EVERY TIME
+$(document).ready(function(){
+
+	// the following are the functions and events
+
+	// these will make the main header disappear for 3000 miliseconds -- 3 seconds, then re-appear.
+
+	$("h1").hide(3000);
+
+	$("h1").show(3000);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// This bracket must ALWAYS close at the bottom of the document ready wrapper
+})
