84e637f7744afe018929b52bd890122d62f78a3b
Kim Kindya <kakindya@gmail.com>
2015-10-13 16:39:03 -0400

    Final tweaks

--- my_library.js
+++ my_library.js
@@ -1,30 +1,30 @@
 // This page demonstrates JQUery anonymousfunctons and events.
 // It's also a cheat sheet.
 
-// THIS IS VERY IMPORTANT to have first EVERY TIME
+// THIS IS VERY IMPORTANT to have first EVERY TIME. It makes sure JavaScript does not run until the page finishes loading.
 $(document).ready(function(){
 
 	// the following are the functions and events
 
-	// these will make the main header disappear for 3000 miliseconds -- 3 seconds, then re-appear.
+	// this pair will make the main header disappear for 3000 miliseconds -- 3 seconds, then re-appear.
 
 	$("h1").hide(3000);
 
 	$("h1").show(3000);
 
-	// These will make the image of jupiter fade in and out over 4 seconds
+	// This pair makes the image of jupiter fade in and out over 4 seconds
 
 	$("img").fadeOut(4000);
 
 	$("img").fadeIn(4000);
 
-	// This makes the first paragraph with pink text hide by sliding upwards, then coming back over 5 seconds
+	// This pair makes the first paragraph with pink text hide by sliding upwards, then coming back over 5 seconds
 
 	$(".para1").slideUp(5000);
 	
 	$(".para1").slideDown(5000);
 
-	// this animates the blue box with the second paragraph, changing its opacity and width over 4 seconds
+	// this pair animates the blue box with the second paragraph, changing its opacity and width over 4 seconds
 
 	$(".para2").animate({
 	opacity: 0.25,
@@ -36,7 +36,7 @@ $(document).ready(function(){
 	width: "50%"
 	} , 4000 );
 
-	// This creates a click event when user clicks on the aqua box 
+	// This makes an alert box appear after a click event, when user clicks on the aqua box 
 
 	$(".clickMe").click(
 	function(){
